4012d25a3869f4209ee8abfef69f5e280a241e84
pjx206 <pjx206@163.com>
2021-02-16 20:24:33 +0800

    update dev tools

--- devtools/compileUI.py
+++ devtools/compileUI.py
@@ -1,8 +1,18 @@
 import os
 import re
 
-def compileAll(uipath: str):
-    files = os.listdir(uipath)
+
+def _filepathFix(filepath):
+    if filepath.find('/'):
+        filepath.replace('/', os.sep)
+    if filepath.find('\\'):
+        filepath.replace('\\', os.sep)
+    return filepath
+
+def compileAll():
+    # TODO 考虑把搜索路径移出去作为命令行解析的一部分
+    searchpath = os.path.join(os.getcwd(), 'ui')
+    files = os.listdir(searchpath)
     to_compile = []
     pat = re.compile(r'.*\.ui')
     for f in files:
@@ -10,10 +20,15 @@ def compileAll(uipath: str):
             to_compile.append(f)
     
     for f in to_compile:
-        compileUi(os.path.join(uipath, f))
+        compileUi(os.path.join(searchpath, f))
 
 def compileUi(filepath: str):
+    filepath = _filepathFix(filepath)
     if os.path.exists(filepath):
-        os.system(f'pyside6-uic -o ui_{filepath}.py {filepath}')
+        fileDirName = os.sep.join(filepath.split(os.sep)[:-1])
+        fileBasenameNoExtension = filepath.split(os.sep)[-1].removesuffix('.ui')
+        cmd = f'pyside6-uic -o {fileDirName}{os.sep}ui_{fileBasenameNoExtension}.py {filepath}'
+        # print(cmd)
+        os.popen(cmd)
     else:
         print('there is no ui files named: ' + filepath)
--- tools.py
+++ tools.py
@@ -3,26 +3,51 @@ import argparse
 import os
 
 
-def parse():
-    parser = argparse.ArgumentParser(description='Dev Tools')
-    parser.add_argument('-rm', choices=['uic', 'pyc'], help='remove certain files')
-    parser.add_argument('-uic', default='--all', help='compile uic files, default to recompile all')
+def rm(args):
+    if args.uic:
+        fileClearUp.removeAllUicFiles()
+    if args.pyc:
+        print('not implemented')
+
+def uic(args):
+    if args.all:
+        compileUI.compileAll()
+    elif args.uifilename is not None:
+        files_dir = os.path.join(os.getcwd(), 'ui') 
+        for f in args.uifilename:
+            if not f.endswith('.ui'):
+                f += '.ui'
+            compileUI.compileUi(os.path.join(files_dir, f))
+
+def main_parse():
+    parser = argparse.ArgumentParser(description="Dev Tools")
+    # parser.add_argument('rm', help='remove specifiled type of files')
+    subparsers = parser.add_subparsers(
+        title='supported commands', help='sub-command help')
+
+    parser_rm = subparsers.add_parser(
+        'rm', help='remove files of specified type')
+    parser_rm.add_argument('--uic', action='store_true',
+                           help='remove all uic compiled files')
+    parser_rm.add_argument('--pyc', action='store_true',
+                           help='remove all pyc files')
+    parser_rm.set_defaults(func=rm)
+
+    parser_uic = subparsers.add_parser('uic', help='compile ui files')
+    parser_uic.add_argument(
+        '-a', '--all', action='store_true', help='compile all ui files')
+    parser_uic.add_argument(dest='uifilename', metavar='filename', nargs='*')
+    parser_uic.set_defaults(func=uic)
 
     return parser.parse_args()
-    
-rm_actions = {
-    'uic': fileClearUp.removeAllUicFiles,
-    'pyc': lambda: print('Not implemented')
-}
+
+
+def main():
+    parse_args = main_parse()
+    # print(parse_args)
+    parse_args.func(parse_args)
+
 
 
 if __name__ == '__main__':
-    args = parse()
-    if args.rm != None:
-        rm_actions[args.rm]()
-    if args.uic != None:
-        uipath = os.path.join(os.getcwd(), 'ui')
-        if args.uic == '--all':
-            compileUI.compileAll(uipath)
-        else:
-            compileUI.compileUi(os.path.join(args.uic))
+    main()
