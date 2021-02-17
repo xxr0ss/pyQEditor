420009717107601f7b89156db214dfbff95cc9ba
pjx206 <pjx206@163.com>
2021-02-17 23:02:55 +0800

    初步实现文件功能
    仍然有不少bug

--- src/codeEditor.py
+++ src/codeEditor.py
@@ -7,6 +7,9 @@ from ui.ui_codeeditor import Ui_CodeEditor
 
 class CodeEditor(QWidget):
     __title__ = 'Code Editor'
+    untitled = 'untitled'
+
+    # TODO: 使得必要的变量为私有，限制访问，比如self.filename
 
     def __init__(self, parent, filename=None):
         super(CodeEditor, self).__init__()
@@ -18,7 +21,7 @@ class CodeEditor(QWidget):
         self.statusBar = self.ui.statusBar
         self.editingArea.cursorPositionChanged.connect(self.update_statusbar_cursor_pos)
 
-        self.filename = filename
+        self.filepath = filename
         if filename is not None:
             self.load_file()
         else:
@@ -26,28 +29,35 @@ class CodeEditor(QWidget):
 
     def load_file(self):
         # if it's None, self.new_file rather than self.load_file should be called
-        assert self.filename is not None
+        assert self.filepath is not None
         # file's existence should be checked before calling this method
-        assert os.path.exists(self.filename)
+        assert os.path.exists(self.filepath)
 
-        with open(self.filename, 'r') as f:
+        with open(self.filepath, 'r') as f:
             content = f.read()
             self.editingArea.setPlainText(content)
-        if '/' in self.filename:
-            self.__title__ = self.filename.split('/')[-1]
-        elif '\\' in self.filename:
-            self.__title__ = self.filename.split('\\')[-1]
+        if '/' in self.filepath:
+            self.__title__ = self.filepath.split('/')[-1]
+        elif '\\' in self.filepath:
+            self.__title__ = self.filepath.split('\\')[-1]
         else:
-            self.__title__ = self.filename
+            self.__title__ = self.filepath
 
-        self.setWindowTitle(self.__title__)
+        self.setWindowTitle(self.filepath)
 
     def new_file(self):
-        self.__title__ = 'untitled'
-        self.setWindowTitle(self.__title__)
+        self.__title__ = self.untitled
+        self.setWindowTitle('new file')
 
     def sizeHint(self) -> QSize:
         return self.parent.size()
+
+    def get_content(self) -> bytes:
+        content = self.editingArea.toPlainText()
+        # TODO 这里要判断吗
+        if isinstance(content, str):
+            content = content.encode()
+        return content
 
     @Slot()
     def update_statusbar_cursor_pos(self):
--- src/mainwindow.py
+++ src/mainwindow.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Qt, Signal, Slot, QCoreApplication, qDebug  # for enum flags
+from PySide6.QtCore import Qt, Signal, Slot, QCoreApplication, qDebug, QObject  # for enum flags
 from PySide6.QtWidgets import QMainWindow, QFileDialog, QDockWidget, QMessageBox, QTabWidget
 from codeEditor import CodeEditor
 from welcomePage import WelcomePage
@@ -19,31 +19,77 @@ class MainWindow(QMainWindow):
         # store editor dock widget instances
         self.dock_pages: list[QDockWidget] = []
 
-        self.externalFile.connect(self.addNewEditor)
+        self.externalFile.connect(self.add_new_dock_editor)
+        # TODO remove dock widget at its closeEvent or something
+        self.current_dock_editor: QDockWidget = None
+
         # set dock tabs position north
         self.setTabPosition(Qt.TopDockWidgetArea, QTabWidget.North)
 
         self.check_cmd_args()
 
     @Slot()
+    def on_actionNew_triggered(self):
+        self.add_new_dock_editor(None)
+
+    @Slot()
     def on_actionOpen_triggered(self):
         filename = QFileDialog.getOpenFileName(self, 'Open File')
-        self.addNewEditor(filename[0])
+        if filename[0] != '':
+            # check if already opened
+            for dock in self.dock_pages:
+                widget = dock.widget()
+                if not isinstance(widget, CodeEditor):
+                    continue
+                if widget.filepath == filename[0]:
+                    # same file opened
+                    return
+
+            self.add_new_dock_editor(filename[0])
     
     @Slot()
     def on_actionSave_triggered(self):
-        pass
+        if self.current_dock_editor is None:
+            print('[x] current editor is none')
+            return
 
-    def addNewEditor(self, filename):
-        editor = CodeEditor(self, filename)
+        editor: CodeEditor = self.current_dock_editor.widget()
+
+        if editor.__title__ == CodeEditor.untitled:
+            # new file to save
+            name = QFileDialog.getSaveFileName(self, 'Save File')
+            if name[0] == '':
+                return
+            filepath = name[0]
+        else:
+            filepath = editor.filepath
+
+        with open(filepath, 'wb') as f:
+            f.write(editor.get_content())
+        editor.filepath = filepath
+        # TODO update editor title
+
+    # @Slot(bool)
+    def update_dock_status(self, visible: bool):
+        sender: QDockWidget = self.sender()
+        if visible:
+            self.current_dock_editor: CodeEditor = sender.widget()
+            print(self.current_dock_editor.__title__)
+
+    def add_new_dock_editor(self, filepath):
+        editor = CodeEditor(self, filepath)
         new_dock_editor = QDockWidget(editor.__title__, self)
         new_dock_editor.setWidget(editor)
         self.addDockWidget(Qt.TopDockWidgetArea, new_dock_editor)
         if len(self.dock_pages) != 0:
             self.tabifyDockWidget(self.dock_pages[-1], new_dock_editor)
         self.dock_pages.append(new_dock_editor)
+
+        new_dock_editor.visibilityChanged[bool].connect(self.update_dock_status)
+
         new_dock_editor.show()
         new_dock_editor.raise_()
+        self.current_dock_editor = new_dock_editor
 
     def add_welcome_page(self):
         # TODO: add welcome page (like vscode)
