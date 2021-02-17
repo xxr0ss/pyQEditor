853f6301efffddc44092b3a8b2b9ea7e215ad55b
pjx206 <pjx206@163.com>
2021-02-17 20:44:28 +0800

    初步实现多Tab编辑页面

--- src/codeEditor.py
+++ src/codeEditor.py
@@ -1,3 +1,4 @@
+import os
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import QSize, Slot
 
@@ -7,7 +8,7 @@ from ui.ui_codeeditor import Ui_CodeEditor
 class CodeEditor(QWidget):
     __title__ = 'Code Editor'
 
-    def __init__(self, parent, content=None):
+    def __init__(self, parent, filename=None):
         super(CodeEditor, self).__init__()
         self.parent = parent
         self.ui = Ui_CodeEditor()
@@ -17,8 +18,33 @@ class CodeEditor(QWidget):
         self.statusBar = self.ui.statusBar
         self.editingArea.cursorPositionChanged.connect(self.update_statusbar_cursor_pos)
 
-        if content is not None:
+        self.filename = filename
+        if filename is not None:
+            self.load_file()
+        else:
+            self.new_file()
+
+    def load_file(self):
+        # if it's None, self.new_file rather than self.load_file should be called
+        assert self.filename is not None
+        # file's existence should be checked before calling this method
+        assert os.path.exists(self.filename)
+
+        with open(self.filename, 'r') as f:
+            content = f.read()
             self.editingArea.setPlainText(content)
+        if '/' in self.filename:
+            self.__title__ = self.filename.split('/')[-1]
+        elif '\\' in self.filename:
+            self.__title__ = self.filename.split('\\')[-1]
+        else:
+            self.__title__ = self.filename
+
+        self.setWindowTitle(self.__title__)
+
+    def new_file(self):
+        self.__title__ = 'untitled'
+        self.setWindowTitle(self.__title__)
 
     def sizeHint(self) -> QSize:
         return self.parent.size()
--- src/mainwindow.py
+++ src/mainwindow.py
@@ -1,46 +1,62 @@
-from PySide6.QtCore import Qt, Signal, Slot  # for enum flags
-from PySide6.QtWidgets import QMainWindow, QFileDialog, QDockWidget, QTabWidget
+from PySide6.QtCore import Qt, Signal, Slot, QCoreApplication, qDebug  # for enum flags
+from PySide6.QtWidgets import QMainWindow, QFileDialog, QDockWidget, QMessageBox, QTabWidget
 from codeEditor import CodeEditor
+import os
 
 from ui.ui_mainwindow import Ui_mainWindow
 
 
 class MainWindow(QMainWindow):
+    # Signals
+    externalFile = Signal(str)
+
     def __init__(self):
         super().__init__()
         self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
 
+        # store editor dock widget instances
+        self.dock_pages: list[QDockWidget] = []
+
+        self.externalFile.connect(self.addNewEditor)
         # set dock tabs position north
         self.setTabPosition(Qt.TopDockWidgetArea, QTabWidget.North)
 
-        self.codeEditor = CodeEditor(self)
-        self.dock_editor = QDockWidget(self.codeEditor.__title__, self)
-        self.dock_editor.setWidget(self.codeEditor)
-
-        self.addDockWidget(Qt.TopDockWidgetArea, self.dock_editor)
-
-
-    def addNewEditor(self, content):
-        editor = CodeEditor(self, content)
-        new_dock_editor = QDockWidget('opened file')
-        new_dock_editor.setWidget(editor)
-        self.addDockWidget(Qt.TopDockWidgetArea, new_dock_editor)
-        self.tabifyDockWidget(new_dock_editor, self.dock_editor)
-
-        # use both show and raise_ to display newly added dock widget in the front
-        new_dock_editor.show()
-        new_dock_editor.raise_()
-
+        self.check_cmd_args()
 
     @Slot()
     def on_actionOpen_triggered(self):
         filename = QFileDialog.getOpenFileName(self, 'Open File')
-        file = filename[0]
-        with open(file, 'r') as f:
-            content = f.read()
-        self.addNewEditor(content)
+        self.addNewEditor(filename[0])
     
     @Slot()
     def on_actionSave_triggered(self):
         pass
+
+    def addNewEditor(self, filename):
+        editor = CodeEditor(self, filename)
+        new_dock_editor = QDockWidget(editor.__title__, self)
+        new_dock_editor.setWidget(editor)
+        self.addDockWidget(Qt.TopDockWidgetArea, new_dock_editor)
+        if len(self.dock_pages) != 0:
+            self.tabifyDockWidget(self.dock_pages[-1], new_dock_editor)
+        self.dock_pages.append(new_dock_editor)
+        new_dock_editor.show()
+        new_dock_editor.raise_()
+
+    def add_welcome_page(self):
+        # TODO: add welcome page (like vscode)
+        pass
+
+    def check_cmd_args(self):
+        argv = QCoreApplication.arguments()
+        if len(argv) > 1:
+            if os.path.exists(argv[1]):
+                qDebug(b'opening file from command line (could be file drop on executable)')
+                self.externalFile.emit(argv[1])
+            else:
+                QMessageBox.information(self, 'Error', 'Invalid file', QMessageBox.Ok)
+        else:
+            # create untitled new file
+            # self.addNewEditor(None)
+            self.add_welcome_page()
