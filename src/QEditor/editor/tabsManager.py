f306dd0258ca3cc89a1d7c8432c34f44c1f4cd11
pjx206 <pjx206@163.com>
2021-02-18 18:38:43 +0800

    增加文件保存功能

---
+++ src/QEditor/editor/tabsManager.py
@@ -0,0 +1,69 @@
+from PySide6.QtCore import QObject, Slot
+from PySide6.QtWidgets import QWidget, QTabWidget, QMessageBox
+from ..welcomePage import WelcomePage
+from ..editor.codeEditorWidget import CodeEditorWidget
+
+
+class TabsManager(QObject):
+    modified_flag = ' *'
+
+    def __init__(self, parent):
+        super(TabsManager, self).__init__()
+        self.parent = parent
+        self._tabs = QTabWidget()
+        self._tabs.setTabPosition(QTabWidget.North)
+        self._tabs.setTabsClosable(True)  # so that there will be a 'X' on tab for closing
+        self._tabs.tabCloseRequested.connect(self.remove_editor_tab)
+
+    @property
+    def tabs(self):
+        return self._tabs
+
+    def add_editor_tab(self, widget: QWidget, title: str):
+        # auto remove welcome page by default when opened new tab
+        if self._tabs.count() == 1:
+            if isinstance(self._tabs.widget(0), WelcomePage):
+                self._tabs.removeTab(0)
+        if isinstance(widget, CodeEditorWidget):
+            # mark tab as modified when content of editor changed
+            widget.content_status_changed.connect(lambda need_saving: self.update_tab_status(widget, need_saving))
+            self._tabs.addTab(widget, title)
+        elif isinstance(widget, WelcomePage):
+            self._tabs.addTab(widget, title)
+
+    @Slot()
+    def remove_editor_tab(self, index):
+        tab = self._tabs.widget(index)
+        if isinstance(tab, CodeEditorWidget):
+            if tab.need_saving:
+                button = QMessageBox.warning(tab, 'Close tab', 'Do you want to save changes?',
+                                             QMessageBox.Ok | QMessageBox.No | QMessageBox.Cancel)
+                if button == QMessageBox.Ok:
+                    self.parent.on_actionSave_triggered()
+                    if tab.need_saving:
+                        # opened save file fileDialog but not actually saved
+                        QMessageBox.information(tab, 'Information', 'File not saved', QMessageBox.Ok)
+                        return
+                elif button == QMessageBox.No:
+                    pass
+                elif button == QMessageBox.Cancel:
+                    return
+                else:
+                    pass
+                    # it's not possible to reach here right?
+
+        self._tabs.removeTab(index)
+
+    @Slot()
+    def update_tab_status(self, w: CodeEditorWidget, need_saving: bool):
+        idx = self._tabs.indexOf(w)
+        text = self._tabs.tabText(idx)
+        if need_saving:
+            if text.endswith(self.modified_flag):
+                return
+            text += self.modified_flag
+        else:
+            # because this slot is called when code editor changes, so there's
+            # no need to set its 'need_saving' status back to False
+            text.removesuffix(self.modified_flag)
+        self._tabs.setTabText(idx, text)
--- src/QEditor/mainwindow.py
+++ src/QEditor/mainwindow.py
@@ -2,6 +2,7 @@ from PySide6.QtCore import Qt, Signal, Slot, QCoreApplication, qDebug  # for enu
 from PySide6.QtWidgets import QMainWindow, QFileDialog, QDockWidget, QMessageBox, QTabWidget, QWidget
 from .editor.codeEditorWidget import CodeEditorWidget
 from .welcomePage import WelcomePage
+from .editor.tabsManager import TabsManager
 import os
 
 from .ui.ui_mainwindow import Ui_mainWindow
@@ -17,37 +18,44 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         self.editor_tabs_dock = QDockWidget('', self)
-        self.editor_tabs = QTabWidget(self)
-        self.editor_tabs_dock.setWidget(self.editor_tabs)
+
+        self.tabs_manager = TabsManager(parent=self)
+        tabs = self.tabs_manager
+        self.editor_tabs_dock.setWidget(self.tabs_manager.tabs)
         self.editor_tabs_dock.resize(self.size())
-        self.editor_tabs.setTabPosition(QTabWidget.North)
+
         self.addDockWidget(Qt.TopDockWidgetArea, self.editor_tabs_dock)
 
         self.externalFile.connect(
-            lambda filepath: self.add_editor_tab(t := CodeEditorWidget(self.editor_tabs_dock, filepath=filepath), t.get_file_base_name()))
+            lambda filepath: tabs.add_editor_tab(
+                t := CodeEditorWidget(parent=self.editor_tabs_dock, filepath=filepath),
+                t.get_file_base_name()))
         self.check_cmd_args()
 
     @Slot()
     def on_actionNew_triggered(self):
-        new_editor = CodeEditorWidget(self.editor_tabs_dock, filepath=None)
-        self.add_editor_tab(new_editor, new_editor.get_file_base_name())
+        new_editor = CodeEditorWidget(parent=self.editor_tabs_dock, filepath=None)
+        self.tabs_manager.add_editor_tab(new_editor, new_editor.get_file_base_name())
 
     @Slot()
     def on_actionOpen_triggered(self):
         filename = QFileDialog.getOpenFileName(self, 'Open File')
+        tabs = self.tabs_manager.tabs
         if filename[0] != '':
             # check if already opened
-            for i in range(self.editor_tabs.count()):
-                page = self.editor_tabs.widget(i)
+            for i in range(tabs.count()):
+                page = tabs.widget(i)
                 if isinstance(page, CodeEditorWidget):
                     if page.filepath == filename[0]:
                         return
 
-            self.add_editor_tab(ce := CodeEditorWidget(self.editor_tabs_dock, filename[0]), ce.get_file_base_name())
+            self.tabs_manager.add_editor_tab(ce := CodeEditorWidget(self.editor_tabs_dock, filename[0]),
+                                             ce.get_file_base_name())
 
     @Slot()
     def on_actionSave_triggered(self):
-        editor = self.editor_tabs.currentWidget()
+        tabs = self.tabs_manager.tabs
+        editor = tabs.currentWidget()
         if not isinstance(editor, CodeEditorWidget):
             return
 
@@ -63,18 +71,13 @@ class MainWindow(QMainWindow):
         with open(filepath, 'wb') as f:
             f.write(editor.get_content())
         editor.filepath = filepath
-        self.editor_tabs.setTabText(self.editor_tabs.currentIndex(), editor.get_file_base_name())
-
-    def add_editor_tab(self, widget: QWidget, title: str):
-        if self.editor_tabs.count() == 1:
-            # remove welcome page when new pages opened
-            if isinstance(self.editor_tabs.widget(0), WelcomePage):
-                self.editor_tabs.removeTab(0)
-        self.editor_tabs.addTab(widget, title)
+        tabs.setTabText(tabs.currentIndex(), editor.get_file_base_name())
 
     def add_welcome_page(self):
+        # FIXME 启动的时候不显示
         page = WelcomePage(self)
-        self.add_editor_tab(page, 'Welcome')
+        page.resize(self.size())
+        self.tabs_manager.add_editor_tab(page, 'Welcome')
 
     def check_cmd_args(self):
         argv = QCoreApplication.arguments()
